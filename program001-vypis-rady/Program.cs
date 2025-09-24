506a15d1bfe4ff6b80011fdd405d462e8a2c3291
Kingu568 <walter.wolf@tul.cz>
2025-09-24 10:17:11 +0200

    Ukol 3, cely program

---
+++ program001-vypis-rady/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace program001_vypis_rady;
+class Program
+{
+    static void Main(string[] args)
+    {   
+        Console.Write("První hodnota: ");
+        int first = int.Parse(Console.ReadLine());
+
+        Console.Write("Poslední hodnotu: ");
+        int last = int.Parse(Console.ReadLine());
+
+        Console.Write("Velikost kroku : ");
+        int step = int.Parse(Console.ReadLine());
+
+        for (int i = first; i <= last; i += step)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
