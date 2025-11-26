8762e4ec8a3a11a636d2600c6d3fc010f4ede272
WalterWolf <walter.wolf@tul.cz>
2025-11-26 11:49:58 +0100

    Ukol 011 - 10to2

---
+++ IS-Programy/program009a-10to2/Program.cs
@@ -0,0 +1,52 @@
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("****************************");
+    Console.WriteLine("**** Převod do binární *****");
+    Console.WriteLine("****************************");
+    Console.WriteLine("******* Walter Wolf ********");
+    Console.WriteLine("****************************");
+    Console.WriteLine();
+
+    Console.Write("Zadejte číslo (celé číslo): ");
+    int dec;
+    int[] bin = new int[32];
+    while (!int.TryParse(Console.ReadLine(), out dec))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte hodnotu znovu: ");
+    }
+
+    if (dec == 0)
+    {
+        Console.WriteLine("Binárně: 0");
+    }
+    else
+    {
+        int number = Math.Abs(dec);
+        int index = 0;
+
+        
+        while (number > 0)
+        {
+            bin[index] = number % 2;   
+            number /= 2;
+            index++;
+        }
+
+        
+        if (dec < 0) Console.Write("-");
+
+        Console.Write("Binárně: ");
+
+        for (int i = index - 1; i >= 0; i--)
+        {
+            Console.Write(bin[i]);
+        }
+        Console.WriteLine();
+    }
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a.");
+    again = Console.ReadLine();
+}
