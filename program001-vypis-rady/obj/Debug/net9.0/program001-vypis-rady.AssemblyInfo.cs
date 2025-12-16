5ae87acddd2beccaf5a95716d031023fc5978513
WalterWolf <walter.wolf@tul.cz>
2025-12-16 13:02:35 +0100

    Update assembly info and cache for new build
    Regenerated AssemblyInfo and cache files to reflect the latest build changes. No source code modifications included.

--- program001-vypis-rady/obj/Debug/net9.0/program001-vypis-rady.AssemblyInfo.cs
+++ program001-vypis-rady/obj/Debug/net9.0/program001-vypis-rady.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("program001-vypis-rady")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+699249d95fbf3dde3b79cf9eee2a8bc1a9fa958e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f98e2c43188d3562a6a2d73b37a318e636f13dfe")]
 [assembly: System.Reflection.AssemblyProductAttribute("program001-vypis-rady")]
 [assembly: System.Reflection.AssemblyTitleAttribute("program001-vypis-rady")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
